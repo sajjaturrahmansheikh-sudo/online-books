cb6942f3110279c98e7434ead85e5f4fe3f9c4a6
sajjaturrahmansheikh-sudo <sajjaturrahmansheikh@gmail.com>
2026-05-07 00:25:03 +0600

    added update user

--- src/app/my-profile/page.jsx
+++ src/app/my-profile/page.jsx
@@ -1,9 +1,34 @@
+'use client'
+import { UpdateProfile } from '@/components/shared/UpdateProfile';
+import { authClient } from '@/lib/auth-client';
+import { Avatar, Card } from '@heroui/react';
 import React from 'react';
 
 const MyProfilePage = () => {
+
+    const userData = authClient.useSession();
+    const user = userData.data?.user;
+
+
+
+
     return (
-        <div>
-            myProfile
+        <div className='container mx-auto min-h-[80vh] mt-10 '>
+            <Card className='text-center space-y-2'>
+                <Avatar className='h-40 w-40 mx-auto'>
+                    <Avatar.Image alt="John Doe" src={user?.image}
+                        referrerPolicy='no-referrer'
+                    />
+                    <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+                </Avatar>
+
+                <h2 className='text-2xl font-bold'> {user?.name} </h2>
+                <p className='text-gray-400 font-medium'> {user?.email} </p>
+
+                <div className='mx-auto'>
+                    <UpdateProfile />
+                </div>
+            </Card>
         </div>
     );
 };
